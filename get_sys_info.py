2b3da2c4895a8232dc237d17cde6d3392d12f2de
Sam Ueckert <Samuel.ueckert@cdw.com>
2024-07-16 17:52:57 +0000

    get sysinfo update

---
+++ get_sys_info.py
@@ -0,0 +1,12 @@
+from paloaltosdk import PanoramaAPI
+
+import os
+from pprint import pprint
+
+pano = PanoramaAPI()
+pano.IP = os.environ['PANORAMA']
+pano.Username = os.environ['CDWU']
+pano.Password = os.environ['CDWP']
+pano.login()
+
+print(pano.get_sys_info('026701009351'))
