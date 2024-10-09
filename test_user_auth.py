850444402b347cc4fcde18dd032baf6423bc4e83
Sam Ueckert <Samuel.ueckert@cdw.com>
2024-10-09 18:27:01 +0000

    Added mock

--- test_user_auth.py
+++ test_user_auth.py
@@ -3,10 +3,10 @@ from login_ui import login_ui
 
 
 def main():
-    # if st.session_state.get("authenticated", False):
-    #     st.title("Main Page: Welcome, " + st.session_state["display_name"])
-    # else:
-    login_ui()
+    if st.session_state.get("authenticated", False):
+        st.title("Main Page: Welcome, " + st.session_state["display_name"])
+    else:
+        login_ui()
 
 
 if __name__ == "__main__":
