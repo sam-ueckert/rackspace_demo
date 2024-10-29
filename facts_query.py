966945cbf411b97072c4ffe86b4fc5b5039a2540
Sam Ueckert <Samuel.ueckert@cdw.com>
2024-10-29 03:35:44 +0000

    update to facts api call for paging

--- facts_query.py
+++ facts_query.py
@@ -7,6 +7,8 @@ load_dotenv("./.env")
 settings = toml.load("settings.toml")
 
 TOKEN_URL = settings['FACTS_TOKEN_URL']
+FACTS_URL = settings['FACTS_URL']
+PER_PAGE = settings['PER_PAGE']
 USERNAME = os.environ['SSO_UNAME']
 PASSWORD = os.environ['SSO_PW']
 
@@ -35,3 +37,29 @@ def get_token(username, password, domain_name, token_url):
     else:
         response.raise_for_status()
 
+
+def get_facts(token, url):
+    '''Get first page of facts from the API'''
+    headers = {
+        "X-Auth-Token": token
+    }
+    endpoint = url + f"?page_no=1&per_page={PER_PAGE}"
+    response = requests.get(endpoint, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        response.raise_for_status()
+
+
+def page_facts(token, url, firstpage):
+    '''Get all pages of facts from the API'''
+    current_page = firstpage['page_no']+1
+    total_pages = firstpage['total_pages']
+    device_list = list(firstpage['Data'])
+    while current_page < total_pages:
+        facts = get_facts(token, url + f"?page={current_page}")
+        current_page += 1
+        device_list.append(facts['Data'])
+
+token = get_token(USERNAME, PASSWORD, "Rackspace", TOKEN_URL)['id']
