ed9bc3556198f1a6b75b93f5349a1ea3aad0026d
Sam Ueckert <Samuel.ueckert@cdw.com>
2024-10-29 03:52:51 +0000

    update facts query for json export

--- facts_query.py
+++ facts_query.py
@@ -1,6 +1,7 @@
 import requests
 import toml
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv("./.env")
@@ -11,6 +12,7 @@ FACTS_URL = settings['FACTS_URL']
 PER_PAGE = settings['PER_PAGE']
 USERNAME = os.environ['SSO_UNAME']
 PASSWORD = os.environ['SSO_PW']
+DB_PATH = settings['DB_PATH']
 
 
 def get_token(username, password, domain_name, token_url):
@@ -43,8 +45,8 @@ def get_facts(token, url):
     headers = {
         "X-Auth-Token": token
     }
-    endpoint = url + f"?page_no=1&per_page={PER_PAGE}"
-    response = requests.get(endpoint, headers=headers)
+    # endpoint = url + f"?page_no=1&per_page={PER_PAGE}"
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         return response.json()
@@ -52,14 +54,30 @@ def get_facts(token, url):
         response.raise_for_status()
 
 
-def page_facts(token, url, firstpage):
+def page_facts(token, url, start_page):
     '''Get all pages of facts from the API'''
-    current_page = firstpage['page_no']+1
-    total_pages = firstpage['total_pages']
-    device_list = list(firstpage['Data'])
+    current_page = start_page['page_no']+1
+    total_pages = start_page['total_pages']
+    device_list = list(start_page['Data'])
     while current_page < total_pages:
         facts = get_facts(token, url + f"?page={current_page}")
         current_page += 1
         device_list.append(facts['Data'])
 
-token = get_token(USERNAME, PASSWORD, "Rackspace", TOKEN_URL)['id']
+
+def save_facts_to_db(device_list):
+    '''Save facts to a local database'''
+    with open(DB_PATH, 'w') as db:
+        json.dumps(device_list, db)
+
+
+if __name__ == "__main__":
+    # Get the token
+    token = get_token(USERNAME, PASSWORD, "Rackspace", TOKEN_URL)['id']
+    # Modify FACTS_URL to include the per_page parameter
+    url = FACTS_URL + f"?page=1&per_page={PER_PAGE}"
+    firstpage = get_facts(token, url)
+    # Page through devices
+    device_list = page_facts(token, FACTS_URL, firstpage)
+    # Save devices to local database
+    save_facts_to_db(device_list)
